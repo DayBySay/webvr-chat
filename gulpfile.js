37c87f028b3b6a48cb0cc2376fd6f081b2b6ec24
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-08 03:15:29 +0900

    gulpからbrowserifyを使えるようにした

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
 
 gulp.task("js", function () {
     gulp.src(["src/**/*.js", "!public_html/js/min/**/*.js"]).pipe(uglify()).pipe(gulp.dest("public_html/js/min"));
+});
+
+
+gulp.task("browserify", function () {
+    return browserify("./src/main.js").bundle().pipe(source("bundle.js")).pipe(gulp.dest("public_html/js"));
 });
