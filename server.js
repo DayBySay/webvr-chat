b504c4e67ae3023bd890fd37d60710cabc69cdf0
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-08 00:15:13 +0900

    ディレクトリ整理

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
   , http = require('http')
   , app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public_html"));
 var port = process.env.PORT || 5000;
 var server = http.createServer(app).listen(port);
 
