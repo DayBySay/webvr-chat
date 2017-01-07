b42e839874935f1021f0231c4d8151437187fc56
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-07 19:27:02 +0900

    モデル修正

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-AFRAME.registerComponent('make-players', {
-    init: function () {
-        makePlayers();
-    }
-});
-
 var playerIDs = ["hoge", "fuga"];
-var Players = new Object;
+var players = new Object;
 
-function makePlayers() {
-    playerIDs.forEach(function (val, index, arr) {
-        var player = makePlayer(val);
-        Players[val] = player;
-    });
+function Player(id) {
+    this.id = id;
+    this.loc = {"x": 0, "y": 0, "z": 0};
 }
 
-function makePlayer(name) {
-    var player = new Player(name);
+function playersWithPlayerIDs(playerIDs) {
+    var players = new Object;
+    playerIDs.forEach(function (val, index, arr) {
+        var player = createPlayerWithPlayerID(val);
+        players[val] = player;
+    });
+
+    return players;
+}
+
+function createPlayerWithPlayerID(id) {
+    var player = new Player(id);
     console.log(player);
     return player;
 }
 
-function Player(name) {
-    this.name = name;
-    this.loc = {"x": 0, "y": 0, "z": 0};
-}
+AFRAME.registerComponent('make-players', {
+    init: function () {
+        players =  playersWithPlayerIDs(playerIDs);
+    }
+});
+
