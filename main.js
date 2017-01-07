400a45ca57bb5f95e61c9466d4cefa1a55d8c297
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-07 19:12:12 +0900

    PlayerIDの一覧からPlayerのオブジェクトを生成するところまで

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-AFRAME.registerComponent('make-player', {
+AFRAME.registerComponent('make-players', {
     init: function () {
+        makePlayers();
     }
 });
+
+var playerIDs = ["hoge", "fuga"];
+var Players = new Object;
+
+function makePlayers() {
+    playerIDs.forEach(function (val, index, arr) {
+        var player = makePlayer(val);
+        Players[val] = player;
+    });
+}
+
+function makePlayer(name) {
+    var player = new Player(name);
+    console.log(player);
+    return player;
+}
+
+function Player(name) {
+    this.name = name;
+    this.loc = {"x": 0, "y": 0, "z": 0};
+}
