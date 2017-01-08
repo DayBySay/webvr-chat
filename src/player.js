3a1d2129703b59699e03b57b4e2a4460251140fa
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-08 09:26:34 +0900

    lint（-人-）

--- src/player.js
+++ src/player.js
@@ -1,7 +1,7 @@
 export default class Player {
     constructor(id) {
-        this.id = id;
-        this.position = {"x": 0, "y": 1.6, "z": 0};
-        this.rotation = {"x": 0, "y": 0, "z": 0};
+        this.id = id
+        this.position = {'x': 0, 'y': 1.6, 'z': 0}
+        this.rotation = {'x': 0, 'y': 0, 'z': 0}
     }
 }
