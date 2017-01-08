01bc88e08bddbf4dac39f255a5b4d6efda253e76
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-08 09:08:49 +0900

    babeling

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,36 @@
 var gulp = require("gulp");
-var uglify = require("gulp-uglify");
 var browserify = require("browserify");
+var babelify = require("babelify");
+var uglify = require("gulp-uglify");
 var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
 
 gulp.task("js-dev", function () {
-    return browserify("./src/main.js")
+    return browserify({
+        entries: "./src/main.js",
+        extensions: [".js"]
+    })
+        .transform(babelify)
         .bundle()
+        .on("error", function (err) {
+            console.log("error: " + err.message);
+            this.emit("end");
+        })
         .pipe(source("bundle.js"))
         .pipe(gulp.dest("public_html/js"));
 });
 
 gulp.task("js", function () {
-    return browserify("./src/main.js")
+    return browserify({
+        entries: "./src/main.js",
+        extensions: [".js"]
+    })
+        .transform(babelify)
         .bundle()
+        .on("error", function (err) {
+            console.log("error: " + err.message);
+            this.emit("end");
+        })
         .pipe(source("bundle.js"))
         .pipe(buffer())
         .pipe(uglify())
