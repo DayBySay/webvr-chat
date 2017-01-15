5a361c61155e00c48fabdb6c79ac1aaf11a212f4
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-16 04:48:56 +0900

    席を用意

--- server.js
+++ server.js
@@ -10,8 +10,63 @@ var io = socketio.listen(server);
 
 var players = new Object;
 
+var seats = {
+	"male": [
+		{"id": 0, x: 0, z: 3, on: false},
+		{"id": 1, x: 3, z: 3, on: false},
+		{"id": 2, x: -3, z: 3, on: false}
+	],
+	"female": [
+		{"id": 0, x: 0, z: -3, on: false},
+		{"id": 1, x: 3, z: -3, on: false},
+		{"id": 2, x: -3, z: -3, on: false}
+	]
+}
+
+function shouldAssignSeatsWithGender(gender) {
+	var st = seats[gender];
+	for (id in st) {
+		if (st[id].on) {
+			continue
+		} else {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function getSeatIfPossibleWithGenser(gender) {
+	if (!shouldAssignSeatsWithGender(gender)) {
+		return 
+	}
+
+	var seat;
+
+	var st = seats[gender];
+	for (id in st) {
+		console.log(st[id]);
+		if (st[id].on) {
+			continue;
+		}
+
+		seat = st[id];
+	}
+
+	seat.on = true;
+	return seat;
+}
+
 io.sockets.on('connection', function(socket) {
     socket.on("login", function (player) {
+		var seat = getSeatIfPossibleWithGenser(player.gender);
+		if (seat === undefined) {
+			return
+		}
+
+		player.position.x = seat.x;
+		player.position.z = seat.z;
+		player.seat = seat;
         players[socket.id] = player;
 
         io.to(socket.id).emit("init_users", players);
@@ -29,9 +84,18 @@ io.sockets.on('connection', function(socket) {
     })
 
     socket.on("disconnect", function () {
-        socket.broadcast.emit("logout_other", players[socket.id]);
+		var player = players[socket.id];
+		var st = seats[player.gender];
+		for (id in st) {
+			if (st[id].id === player.seat.id) {
+				st[id].on = false;
+			}
+		}
+
+        socket.broadcast.emit("logout_other", player);
         delete players[socket.id]
         console.log("logout_other");
         console.log(players);
+		console.log(seats);
     })
 });
--- src/user.js
+++ src/user.js
@@ -1,6 +1,8 @@
 export default class User {
     constructor(id) {
         this.id = id
+		this.gender = "male"
+		this.seat
         this.position = {'x': 0, 'y': 1.6, 'z': 0}
         this.rotation = {'x': 0, 'y': 0, 'z': 0}
     }
